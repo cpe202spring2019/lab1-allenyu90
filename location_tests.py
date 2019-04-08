b3e4c3223bfb50b22640eb9402a7d94690fb08ce
Allen Yu <acyu@calpoly.edu>
2019-04-08 12:36:38 -0700

    changes made

--- location_tests.py
+++ location_tests.py
@@ -6,8 +6,18 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
+        self.assertNotEqual(repr(loc),"Location('SF', 45.4, -120.7)")
+
     # Add more tests!
+
+    def test_eq(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location('SF', 45.4, -120.7)
+        loc3 = loc1
+        self.assertEqual(loc1, loc3)
+        self.assertNotEqual(loc1, loc2)
+        self.assertNotEqual(loc2, loc3)
+
 
 if __name__ == "__main__":
         unittest.main()
